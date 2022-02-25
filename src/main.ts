040b597c3192fe20a8a8c5c605c372cd6a220fd6
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2022-02-25 11:02:35 -0300

    chore: set up quasar

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { createApp } from "vue";
+import { Quasar } from "quasar";
+
+// Import icon libraries
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+
+// Import Quasar css
+import "quasar/src/css/index.sass";
+
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+
+app.use(Quasar, {
+  plugins: {},
+});
 
 app.use(router);
 
