d6a43eddac5c3098a4738189e8cc5cb6fe107735
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2022-02-25 11:21:20 -0300

    feat: add toggle drawer button

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,22 @@
 import { ref } from "vue";
 
 const isDrawerOpen = ref(true);
+
+const toggleDrawer = () => {
+  isDrawerOpen.value = !isDrawerOpen.value;
+};
 </script>
 
 <template>
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-dark text-white">
       <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleDrawer" />
+
         <q-toolbar-title> GitHub Dashboard </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="isDrawerOpen" bordered> </q-drawer>
+    <q-drawer show-if-above bordered side="left" v-model="isDrawerOpen" />
   </q-layout>
 </template>
